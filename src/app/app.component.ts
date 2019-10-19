471ff00d1634991fda5dd3335c7b81a968bbf176
Kaleeswaran <kaleeswaran@iinerds.com>
2019-10-19 20:26:01 +0530

    bacic pgm initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'basic-angular-modals';
+  title = "basic-angular-modals";
+  input1;
+  input2;
+  result = 0;
+  save() {
+    this.result = this.input1 + this.input2;
+  }
 }
